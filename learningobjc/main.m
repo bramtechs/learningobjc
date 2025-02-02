a62e8d97a70e442fb90c312566cf902965caa9ac
bramtechs <bramtech@telenet.be>
2025-02-02 12:27:30 +0100

    Initial Commit

---
+++ learningobjc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  learningobjc
+//
+//  Created by Bram on 02/02/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
